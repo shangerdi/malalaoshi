d64211e6afcd4dd60fe6fb6e7f2d03b9630bd105
Liang Sun <i@liangsun.org>
2015-06-16 15:55:54 +0800

    Add profiles.

--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,11 @@
 Meteor.publish('areas', function() {
   return Areas.find();
 });
+Meteor.publish("userData", function () {
+  if (this.userId) {
+    return Meteor.users.find({_id: this.userId},
+                             {fields: {'role': 1}});
+  } else {
+    this.ready();
+  }
+});
