6d654d32eeb010597a7c9d17d5e024aa58789a7c
Liang Sun <i@liangsun.org>
2015-07-01 15:45:22 +0800

    Add mobile config.

---
+++ mobile-config.js
@@ -0,0 +1,12 @@
+App.info({
+  name: '麻辣老师',
+  description: '找到独特的老师，只为独特的你',
+  version: '1.0.0'
+});
+
+App.icons({
+  'android_ldpi': 'resources/icons/icon.png',
+  'android_mdpi': 'resources/icons/icon.png',
+  'android_hdpi': 'resources/icons/icon.png',
+  'android_xhdpi': 'resources/icons/icon.png'
+});
