007f53eba65aa441c400444140e5c483035af095
Liang Sun <i@liangsun.org>
2015-07-01 17:30:55 +0800

    Update mobile config.

--- mobile-config.js
+++ mobile-config.js
@@ -1,6 +1,9 @@
 App.info({
+  id: 'com.malalaoshi.app',
   name: '麻辣老师',
   description: '找到独特的老师，只为独特的你',
+  author: 'Malalaoshi Dev Team',
+  website: 'https://malalaoshi.com',
   version: '1.0.0'
 });
 
@@ -8,10 +11,32 @@ App.icons({
   'android_ldpi': 'resources/icons/icon.png',
   'android_mdpi': 'resources/icons/icon.png',
   'android_hdpi': 'resources/icons/icon.png',
-  'android_xhdpi': 'resources/icons/icon.png'
+  'android_xhdpi': 'resources/icons/icon.png',
+  'iphone': 'resources/icons/icon.png',
+  'iphone_2x': 'resources/icons/icon.png',
+  'iphone_3x': 'resources/icons/icon.png',
+  'ipad': 'resources/icons/icon.png',
+  'ipad_2x': 'resources/icons/icon.png'
 });
 
 App.launchScreens({
   'iphone': 'resources/icons/icon.png',
-  'iphone_2x': 'resources/icons/icon.png'
+  'iphone_2x': 'resources/icons/icon.png',
+  'iphone5': 'resources/icons/icon.png',
+  'iphone6': 'resources/icons/icon.png',
+  'iphone6p_portrait': 'resources/icons/icon.png',
+  'iphone6p_landscape': 'resources/icons/icon.png',
+  'ipad_portrait': 'resources/icons/icon.png',
+  'ipad_portrait_2x': 'resources/icons/icon.png',
+  'ipad_landscape': 'resources/icons/icon.png',
+  'ipad_landscape_2x': 'resources/icons/icon.png',
+  'android_ldpi_portrait': 'resources/icons/icon.png',
+  'android_ldpi_landscape': 'resources/icons/icon.png',
+  'android_mdpi_portrait': 'resources/icons/icon.png',
+  'android_mdpi_landscape': 'resources/icons/icon.png',
+  'android_hdpi_portrait': 'resources/icons/icon.png',
+  'android_hdpi_landscape': 'resources/icons/icon.png',
+  'android_xhdpi_portrait': 'resources/icons/icon.png',
+  'android_xhdpi_landscape': 'resources/icons/icon.png'
 });
+
