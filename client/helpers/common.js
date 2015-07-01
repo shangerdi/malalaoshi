04abaf82f97b6f44693c9ff8964676573e54c9fb
Liang Sun <i@liangsun.org>
2015-07-01 17:31:20 +0800

    Hide footer when Cordova.

--- client/helpers/common.js
+++ client/helpers/common.js
@@ -4,3 +4,9 @@ Template.registerHelper('isAdmin', function(){
 Template.registerHelper('created', function(){
   return moment(this.createdAt).fromNow();
 });
+Template.registerHelper('isCordova', function(){
+  return Meteor.isCordova;
+});
+Template.registerHelper('isNotCordova', function(){
+  return !Meteor.isCordova;
+});
