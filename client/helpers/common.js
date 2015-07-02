75f973b3ad7082f79e440688870839704223ccf3
jackyZhang <15957031@qq.com>
2015-07-02 10:53:58 +0800

    merge common.js

--- client/helpers/common.js
+++ client/helpers/common.js
@@ -4,9 +4,15 @@ Template.registerHelper('isAdmin', function(){
 Template.registerHelper('isParent', function(){
   return Meteor.user() && Meteor.user().role === 'parent';
 });
+Template.registerHelper('isParentOrAdmin', function(){
+  return Meteor.user() && (Meteor.user().role === 'admin' || Meteor.user().role === 'parent');
+});
 Template.registerHelper('created', function(){
   return moment(this.createdAt).fromNow();
 });
-Template.registerHelper('isParentOrAdmin', function(){
-  return Meteor.user() && (Meteor.user().role === 'admin' || Meteor.user().role === 'parent');
+Template.registerHelper('isCordova', function(){
+  return Meteor.isCordova;
+});
+Template.registerHelper('isNotCordova', function(){
+  return !Meteor.isCordova;
 });
