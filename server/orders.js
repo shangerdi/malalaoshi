249d3f5204c21f188629b57cc9d86e9c17f583ec
jackyZhang <15957031@qq.com>
2015-07-13 20:15:35 +0800

    easy code

--- server/orders.js
+++ server/orders.js
@@ -1,12 +1,12 @@
 Meteor.methods({
   updateOrder: function(order) {
     var curUser = Meteor.user();
-    if (!Meteor.user()){
+    if (!curUser){
         throw new Meteor.Error('权限不足', "当前用户权限不足");
     }
-    var updateOrder = _.extend(order, {
+    order = _.extend(order, {
 	     submitUserId: curUser._id,
-       submitted: new Date()
+       submitted: Date.now()
     });
 
     return Orders.update({_id: updateOrder._id}, {$set: updateOrder}, {upsert: true});
