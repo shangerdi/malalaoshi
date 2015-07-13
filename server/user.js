5a9af83e6c7346eff5e1e13f465f3fe01ae02f9f
Liang Sun <i@liangsun.org>
2015-07-13 18:46:14 +0800

    Meteor.methods should in server folder.

--- server/user.js
+++ server/user.js
@@ -16,3 +16,40 @@ Meteor.methods({
     return Meteor.users.update({_id: user.id}, {$set: {"role": user.role}});
   }
 });
+Meteor.methods({
+  updateProfile: function(profile) {
+    check(profile, {
+      name: String,
+      // nickname: String,
+      gender: String,
+      birthday: String,
+      state: String,
+      subjects: Array,
+      // college: String,
+      // degree: String,
+      // major: String,
+      address: Object,
+      selfIntro: String//,
+      // motto: String
+    });
+
+    var errors = validateProfile(profile);
+    if (!!errors.hasError) {
+      throw new Meteor.Error('无效设置', "参数设置错误");
+    }
+
+    var oldProfile = Meteor.user().profile;
+    if (oldProfile) {
+      profile = _.extend(Meteor.user().profile, profile);
+    }
+    // console.log(profile);
+    var setObj = {profile:profile};
+    if (Meteor.user().role=='teacher') {// teachers need to audit
+      var now = Date.now();
+      TeacherAudit.update({userId:Meteor.userId()},{$set:{name:profile.name,submitTime:now,basicInfo:{submitTime:now, status: 'submited'}}},{upsert:true});
+      setObj["status.basic"] = "submited";
+    }
+    Meteor.users.update({_id: Meteor.userId()}, {$set: setObj});
+    // TODO：用户操作日志 UserOpLogs
+  }
+});
