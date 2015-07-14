b3d218c1e585eabe1ed606102d2cce0977a92114
Scott <zhonglijunyi@163.com>
2015-07-14 12:46:00 +0800

    refactor area from subscribe-publish to method-call

--- client/templates/includes/area.js
+++ client/templates/includes/area.js
@@ -1,40 +1,15 @@
 AreaOfChina = function (options) {
-	findArea = function(code) {
-		return Areas.findOne({"code":code});
+	findArea = function(code, callback) {
+		return Meteor.call('findArea', code, callback);
 	}
 
-	getProvince = function() {
-		return Areas.find({"level":1})
+	getProvince = function(callback) {
+		return Meteor.call('getProvince', callback);
 	}
 
-	getSubAreas = function(code) {
-		var theArea = findArea(code);
-		if (!theArea) {
-			return;
-		}
-		if (theArea.level == 1) {
-			return Areas.find({"level":2,"code":{$regex: '^'+theArea.code.substr(0,2)+'.*$', $options: ''}});
-		} else if (theArea.level == 2) {
-			return Areas.find({"level":3,"code":{$regex: '^'+theArea.code.substr(0,4)+'.*$', $options: ''}});
-		}
-		return theArea;
+	getSubAreas = function(code, callback) {
+		return Meteor.call('getSubAreas', code, callback);
 	}
 
 	return {findArea:findArea,getProvince:getProvince,getSubAreas:getSubAreas};
 };
-// Test code, must wait {Meteor.subscribe('areas')} is done;
-// var areaOfChina = new AreaOfChina();
-// var p = areaOfChina.findArea("110000");
-// console.log(p);
-// var p = areaOfChina.getProvince();
-// console.log(p);
-// var p = areaOfChina.getSubAreas("110000");
-// console.log(p);
-// var p = areaOfChina.getSubAreas("210000");
-// console.log(p);
-// var p = areaOfChina.getSubAreas("371500");
-// console.log(p);
-// var p = areaOfChina.getSubAreas("371522");
-// console.log(p);
-// var p = areaOfChina.getSubAreas("410707");
-// console.log(p);
