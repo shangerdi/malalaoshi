90cc09fcf2dcdb48a6bcd262f602a5a9787a26f6
Liang Sun <i@liangsun.org>
2015-07-14 19:55:31 +0800

    Refactor site name; Fix activeRouteClass; Remove unused files.

--- client/templates/includes/header.js
+++ client/templates/includes/header.js
@@ -1,14 +1,4 @@
 Template.header.helpers({
-  activeRouteClass: function(/* route names */) {
-    var args = Array.prototype.slice.call(arguments, 0);
-    args.pop();
-
-    var active = _.any(args, function(name) {
-      return Router.current() && Router.current().route.getName() === name
-    });
-
-    return active && 'active';
-  },
   sup: function() {
     var host = window.location.hostname;
     if (host.match('(localhost|127\.0\.0\.1|.*\.local)')) {
