0e033c453eb9dcfa193d9746442436d2da185b8f
Scott <zhonglijunyi@163.com>
2015-07-16 15:35:51 +0800

    listen new message coming

--- client/templates/includes/header.js
+++ client/templates/includes/header.js
@@ -1,3 +1,4 @@
+var oldMessageCount = 0;
 Template.header.helpers({
   sup: function() {
     var host = window.location.hostname;
@@ -11,6 +12,13 @@ Template.header.helpers({
   },
   messageCount: function(){
     var cnt = Messages.find({userId: Meteor.userId(), read: {$ne: true}}).count();
+    if (oldMessageCount!=cnt) {
+      if (oldMessageCount<cnt) {
+        console.log("new "+(cnt-oldMessageCount)+" messages have come");
+      }
+      oldMessageCount = cnt;
+      console.log("message count is changed");
+    }
     if (cnt > 999) {
       return "···";
     }
