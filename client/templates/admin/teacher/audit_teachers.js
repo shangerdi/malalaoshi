576482ee4d5870475cedc0eeda5f41c7717bad51
Liang Sun <i@liangsun.org>
2015-07-17 23:45:55 +0800

    Use changing event of `select` instead of a dedicated button to control filtering; Use userId instead of _userId

--- client/templates/admin/teacher/audit_teachers.js
+++ client/templates/admin/teacher/audit_teachers.js
@@ -1,7 +1,7 @@
 Template.auditTeachers.helpers({
 });
 Template.auditTeachers.events({
-  'click .btn.btn-search': function(e){
+  'change select[name=state]': function(e){
     var state = $('[name=state]').val();
     Session.set('state', state);
   }
@@ -11,12 +11,9 @@ var convStatus2Str = function(status) {
     return "未提交";
   }
   var statusDict = {'approved':"通过",'submited':"待审核",'rejected':"被驳回"};
-  var str = statusDict[status];
-  if (str) {
-    return str;
-  }
-  return "-";
+  return statusDict[status] || '-';
 }
+
 Template.userAuditItem.helpers({
   submitTime: function() {
     return fromNowReactive(moment(this.submitTime, 'x'));
@@ -58,10 +55,5 @@ Template.userAuditItem.helpers({
       return "审核";
     }
     return "查看";
-  }
-});
-Template.userAuditItem.events({
-  'click .btn-audit': function(e) {
-    Router.go('auditTeacher', {_userId: Template.instance().data.userId});
   }
 });
