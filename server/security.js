5a8ed1d37ad74bff2612883d6dfd146d91e7d3e6
jackyZhang <15957031@qq.com>
2015-07-21 18:51:24 +0800

    order use security

---
+++ server/security.js
@@ -0,0 +1,20 @@
+Security.defineMethod("ifHasRole", {
+  fetch: [],
+  transform: null,
+  deny: function (type, arg, userId, doc) {
+    var curUser = Meteor.user();
+    return !curUser ? true : curUser.role !== arg;
+  }
+});
+
+Security.defineMethod("userOwnOrder", {
+  fetch: [],
+  transform: null,
+  deny: function (type, arg, userId, doc) {
+    return userId !== doc.student.id;
+  }
+});
+
+
+Orders.permit(['insert', 'update', 'remove']).userOwnOrder().apply();
+Orders.permit(['insert', 'update', 'remove']).ifHasRole('admin').apply();
