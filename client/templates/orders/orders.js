6cdd0bb5338039e8cfaf2c7d5360e5a3c55e97bb
jackyZhang <15957031@qq.com>
2015-07-29 17:28:36 +0800

    no order show search teachers

--- client/templates/orders/orders.js
+++ client/templates/orders/orders.js
@@ -1,3 +1,15 @@
 Template.orders.onRendered(function () {
   IonNavigation.skipTransitions = true;
 });
+Template.orders.helpers({
+  empty: function(){
+    return !this.orders || this.orders.count() === 0;
+  }
+});
+Template.orders.events({
+  'click #findTeachers': function(e) {
+    e.preventDefault();
+
+    Router.go("teachers");
+  }
+});
