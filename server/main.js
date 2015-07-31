a64d791d1c5aed7a2e3c8e4d81564bd0be424bec
Liang Sun <i@liangsun.org>
2015-07-31 18:50:32 +0800

    Remove browser-policy temporally.

--- server/main.js
+++ server/main.js
@@ -1,6 +1,8 @@
 Meteor.startup(function () {
+  /*
   BrowserPolicy.content.allowOriginForAll("http://meteor.local");
   BrowserPolicy.content.allowFontDataUrl();
   BrowserPolicy.content.allowImageOrigin('s3.cn-north-1.amazonaws.com.cn');
   BrowserPolicy.content.allowImageOrigin('s3-ap-southeast-1.amazonaws.com');
+  */
 });
