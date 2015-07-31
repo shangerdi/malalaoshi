07e9eb0894d673f7f0540c0dca7c081b84b4d097
Liang Sun <i@liangsun.org>
2015-07-31 19:09:36 +0800

    Add browser policy again.

--- server/main.js
+++ server/main.js
@@ -1,8 +1,11 @@
 Meteor.startup(function () {
-  /*
-  BrowserPolicy.content.allowOriginForAll("http://meteor.local");
+  BrowserPolicy.content.allowOriginForAll("meteor.local");
   BrowserPolicy.content.allowFontDataUrl();
-  BrowserPolicy.content.allowImageOrigin('s3.cn-north-1.amazonaws.com.cn');
-  BrowserPolicy.content.allowImageOrigin('s3-ap-southeast-1.amazonaws.com');
-  */
+  BrowserPolicy.content.allowOriginForAll('s3.cn-north-1.amazonaws.com.cn');
+  BrowserPolicy.content.allowOriginForAll('s3-ap-southeast-1.amazonaws.com');
+  BrowserPolicy.content.allowOriginForAll('api.pingxx.com');
+  BrowserPolicy.content.allowOriginForAll('sissi.pingxx.com');
+  BrowserPolicy.content.allowOriginForAll('wappaygw.alipay.com');
+  BrowserPolicy.content.allowOriginForAll('www.google-analytics.com');
+  BrowserPolicy.content.allowOriginForAll('enginex.kadira.io');
 });
