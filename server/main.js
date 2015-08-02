8ed6269ce718af586a30797a7b8b681c4c72b4c0
Liang Sun <i@liangsun.org>
2015-08-02 23:00:46 +0800

    Revert.

--- server/main.js
+++ server/main.js
@@ -1,7 +1,6 @@
 Meteor.startup(function () {
   BrowserPolicy.content.allowOriginForAll("meteor.local");
   BrowserPolicy.content.allowFontDataUrl();
-  /*
   BrowserPolicy.content.allowOriginForAll('s3.cn-north-1.amazonaws.com.cn');
   BrowserPolicy.content.allowOriginForAll('s3-ap-southeast-1.amazonaws.com');
   BrowserPolicy.content.allowOriginForAll('api.pingxx.com');
@@ -9,5 +8,4 @@ Meteor.startup(function () {
   BrowserPolicy.content.allowOriginForAll('wappaygw.alipay.com');
   BrowserPolicy.content.allowOriginForAll('www.google-analytics.com');
   BrowserPolicy.content.allowOriginForAll('enginex.kadira.io');
-  */
 });
