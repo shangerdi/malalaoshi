520e4b71663b11b7354d10e3743e0be0900c1c23
Liang Sun <i@liangsun.org>
2015-08-02 21:24:07 +0800

    Refactor browser policy.

--- server/main.js
+++ server/main.js
@@ -1,6 +1,7 @@
 Meteor.startup(function () {
   BrowserPolicy.content.allowOriginForAll("meteor.local");
   BrowserPolicy.content.allowFontDataUrl();
+  /*
   BrowserPolicy.content.allowOriginForAll('s3.cn-north-1.amazonaws.com.cn');
   BrowserPolicy.content.allowOriginForAll('s3-ap-southeast-1.amazonaws.com');
   BrowserPolicy.content.allowOriginForAll('api.pingxx.com');
@@ -8,4 +9,5 @@ Meteor.startup(function () {
   BrowserPolicy.content.allowOriginForAll('wappaygw.alipay.com');
   BrowserPolicy.content.allowOriginForAll('www.google-analytics.com');
   BrowserPolicy.content.allowOriginForAll('enginex.kadira.io');
+  */
 });
