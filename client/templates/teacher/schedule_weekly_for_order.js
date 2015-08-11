c90272f070b606d13bf5f059ecf8dd2d9daf8f8f
Scott <zhonglijunyi@163.com>
2015-08-11 10:44:00 +0800

    test schedule weekly time-table for order (continue)

--- client/templates/teacher/schedule_weekly_for_order.js
+++ client/templates/teacher/schedule_weekly_for_order.js
@@ -1,5 +1,6 @@
 var convMinutes2Str = CourseTable.convMinutes2Str;
 Template.scheduleWeeklyForOrder.onCreated(function() {
+  Session.set('errors', {});
   // define cache data
   this.cacheData = this.cacheData || {};
   var teacher = Meteor.users.findOne({_id:Router.current().params.teacherId});
@@ -26,6 +27,12 @@ Template.scheduleWeeklyForOrder.onCreated(function() {
   this.cacheData.timePhases = timePhases.sort(function(a,b){return a.start-b.start;});
 });
 Template.scheduleWeeklyForOrder.helpers({
+  errorMessage: function(field) {
+    return Session.get('errors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('errors')[field] ? 'has-error' : '';
+  },
   lessonCounts: function() {
     var tct = TeacherCourseTables.findOne({"teacher.id": Template.instance().cacheData.teacherId});
     var availablePhases = (tct && tct.phases)?tct.phases:null;
@@ -90,6 +97,41 @@ Template.scheduleWeeklyForOrder.helpers({
 Template.scheduleWeeklyForOrder.events({
   'click td.phase': function(e) {
     var ele=e.target, $ele = $(ele);
-    alert('周'+CourseTable.dayNumWords[$ele.data('weekday')]+"  "+convMinutes2Str($ele.data('start'))+"  "+convMinutes2Str($ele.data('end')));
+    if ($ele.hasClass("available")) {
+      $ele.toggleClass("chosen");
+    }
+  },
+  'click .btn-save': function(e) {
+    var lessonCount = parseInt($("input[name=lessonCount").val()), errors={hasError:false};
+    if (isNaN(lessonCount) || lessonCount<1) {
+      errors.lessonCount="课时数不能小于1";
+      errors.hasError=true;
+    }
+    var phases = [];
+    $("td.phase.chosen").each(function(){
+      $this = $(this), i = $this.data('weekday'), start = $this.data('start'), end = $this.data('end');
+      phases.push({weekday:i, start:start, end:end})
+    });
+    console.log(phases);
+    if(phases.length==0) {
+      errors.phases="请点击上面方格选择时间段";
+      errors.hasError=true;
+    }
+    Session.set('errors', errors);
+    if (errors.hasError) {
+      return;
+    }
+    $(e.currentTarget).addClass("disabled");
+    var teacherId = Template.instance().cacheData.teacherId;
+    Meteor.call('reserveCourses', {'teacherId':teacherId, 'lessonCount':lessonCount, 'phases':phases}, function(err, result) {
+      if(err){
+        errors.lessonCount=err.reason;
+        Session.set('errors', errors);
+        $(e.currentTarget).removeClass("disabled");
+        return throwError(err.reason);
+      }
+      alert("约课成功");
+      $(e.currentTarget).removeClass("disabled");
+    });
   }
 });
---
+++ server/course_table.js
@@ -0,0 +1,91 @@
+Meteor.methods({
+  reserveCourses: function(params) {
+    console.log(params);
+    var curUser = Meteor.user();
+    if (!curUser) {
+      throw new Meteor.Error('权限不足', "需要登录");
+    }
+    var teacherId = params.teacherId, lessonCount = params.lessonCount, phases = params.phases;
+    var teacher = Meteor.users.findOne({"_id": teacherId, role: 'teacher'});
+    if (!teacher) {
+      throw new Meteor.Error('教师不存在', "没有查找到该教师的记录");
+    }
+    if (lessonCount<1 || phases.length==0) {
+      throw new Meteor.Error('课时参数错误', "请核对课时数或选择时间段");
+    }
+    try{
+    console.log("sort phases");
+    // sort phases
+    var sortedPhase = phases.sort(function(a,b){
+      var tmp = a.weekday-b.weekday;
+      if (tmp!=0) return tmp;
+      return a.start-b.start;
+    });
+    console.log(sortedPhase);
+    console.log("calc");
+    // calc key time point: today, timeStamp, the day to start and already reserved list
+    var exDays = CourseTable.experienceDays;// TODO: calculate days to attend experience course.
+    console.log('exDays:' + exDays);
+    var now = new Date();
+    var today = new Date(now.getFullYear(),now.getMonth(),now.getDate());
+    console.log(today);
+    var toStartDay = today.getDay()+exDays;
+    if (toStartDay>7) {
+      toStartDay -= 7;
+    }
+    console.log("toStartDay: "+toStartDay);
+    var firstTime = today.getTime()+exDays*24*60*60*1000;
+    var aWeekLaterTime = today.getTime()+(7+exDays)*24*60*60*1000;
+    console.log("firstTime: "+new Date(firstTime)+", aWeekLaterTime: "+new Date(aWeekLaterTime));
+    // var reservedList = CourseAttendances.find({"teacher.id":teacherId, 'attendDay': {$gte: firstTime, $lt: aWeekLaterTime}}).fetch();
+    reservedList = [];
+    console.log('reservedList: '+reservedList);
+    console.log("loop");
+    // generate new records to attend course
+    var isConflict = false, count=0, weekCount=0, toInsertList=[];
+    while(!isConflict && count<lessonCount) {
+      _.each(sortedPhase, function(phase){
+        if (!isConflict && count<lessonCount) {
+          return;
+        }
+        // find conflict phases
+        var item = _.find(reservedList, function(obj){
+          var tmpDate = new Date(obj.attendDay), weekday = tmpDate.getDay();
+          weekday = (weekday==0?7:weekday);
+          return weekday==phase.weekday && obj.phase.start==phase.start && obj.phase.end==phase.end;
+        });
+        if (item) {
+          isConflict = true;
+          return;
+        }
+        // new phase to attend course
+        var newAttendDayTime;
+        if (toStartDay<=phase.weekday) {
+          newAttendDayTime = firstTime+(phase.weekday-toStartDay+weekCount*7)*24*60*60*1000;
+        } else {
+          newAttendDayTime = firstTime+(7+phase.weekday-toStartDay+weekCount*7)*24*60*60*1000;
+        }
+        toInsertList.push({
+          'teacher':{'id':teacherId,'name':teacher.profile.name},
+          'student':{'id':curUser._id,'name':curUser.profile.name},
+          'attendDay':newAttendDayTime,
+          'phase':{'start':phase.start,'end':phase.end},
+          'state':"reserved"
+        });
+        count++;
+      });
+      weekCount++;
+      console.log(weekCount);
+    }
+    console.log(toInsertList);
+    if (isConflict) {
+      throw new Meteor.Error('时间冲突', "您选择的上课时间和别人冲突了，请确认！");
+    }
+    // insert
+    CourseAttendances.insert(toInsertList);
+  }catch(ex) {
+    console.log(ex);
+    throw new Meteor.Error('500', "系统错误请稍后重试");
+  }
+  }
+});
