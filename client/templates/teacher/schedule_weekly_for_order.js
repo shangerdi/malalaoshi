4a4503f5d60d72ac24c346cade69dc987a1d67f8
Scott <zhonglijunyi@163.com>
2015-08-18 17:49:02 +0800

    refactor schedule-weekly-for-order table, and it into teacher info page

--- client/templates/teacher/schedule_weekly_for_order.js
+++ client/templates/teacher/schedule_weekly_for_order.js
@@ -1,9 +1,8 @@
 var convMinutes2Str = ScheduleTable.convMinutes2Str;
 Template.scheduleWeeklyForOrder.onCreated(function() {
-  Session.set('errors', {});
   // define cache data
   this.cacheData = this.cacheData || {};
-  var teacher = Meteor.users.findOne({_id:Router.current().params.teacherId});
+  var teacher = Meteor.users.findOne({_id:this.data.teacherId});
   if (!Meteor.user() || !teacher) {
     this.cacheData.notLogin = true;
     return;
@@ -27,17 +26,6 @@ Template.scheduleWeeklyForOrder.onCreated(function() {
   this.cacheData.timePhases = timePhases.sort(function(a,b){return a.start-b.start;});
 });
 Template.scheduleWeeklyForOrder.helpers({
-  errorMessage: function(field) {
-    return Session.get('errors')[field];
-  },
-  errorClass: function (field) {
-    return !!Session.get('errors')[field] ? 'has-error' : '';
-  },
-  lessonCounts: function() {
-    var tct = TeacherAvailableTimes.findOne({"teacher.id": Template.instance().cacheData.teacherId});
-    var availablePhases = (tct && tct.phases)?tct.phases:null;
-    return availablePhases?availablePhases.length:0;
-  },
   timePhases: function() {
     return Template.instance().cacheData.timePhases;
   },
@@ -65,73 +53,7 @@ Template.scheduleWeeklyForOrder.helpers({
         return 'available';
       }
     } else {
-      return 'un-available';
+      return 'unavailable';
     }
-  },
-  getPhaseText: function(i, start, end) {
-    var teacherId = Template.instance().cacheData.teacherId, tct = TeacherAvailableTimes.findOne({"teacher.id": teacherId});
-    var availablePhases = (tct && tct.phases)?tct.phases:null;
-    var tmp = _.find(availablePhases, function(obj){
-      return obj.weekday==i && obj.phase.start==start && obj.phase.end==end;
-    });
-    if (tmp) {
-      var exDays = ScheduleTable.tryDays;// TODO: calculate days to attend experience course.
-      var reservedList = ScheduleTable.getWeeklyTeacherReservedList(teacherId, new Date(), exDays);
-      var item = _.find(reservedList, function(obj){
-        return obj.weekday==i && obj.phase.start==start && obj.phase.end==end;
-      });
-      if (item) {
-        return 'r';
-      } else {
-        return 'a';
-      }
-    } else {
-      return 'u';
-    }
-  }
-});
-Template.scheduleWeeklyForOrder.events({
-  'click td.phase': function(e) {
-    var ele=e.target, $ele = $(ele);
-    if ($ele.hasClass("available") && !$ele.hasClass('reserved')) {
-      $ele.toggleClass("chosen");
-    }
-  },
-  'click .btn-save': function(e) {
-    var lessonCount = parseInt($("input[name=lessonCount").val()), errors={hasError:false};
-    if (isNaN(lessonCount) || lessonCount<1) {
-      errors.lessonCount="课时数不能小于1";
-      errors.hasError=true;
-    }
-    var phases = [];
-    $("td.phase.chosen").each(function(){
-      $this = $(this);
-      if ($this.hasClass('reserved')) {
-        return;
-      }
-      i = $this.data('weekday'), start = $this.data('start'), end = $this.data('end');
-      phases.push({weekday:i, start:start, end:end})
-    });
-    console.log(phases);
-    if(phases.length==0) {
-      errors.phases="请点击上面方格选择时间段";
-      errors.hasError=true;
-    }
-    Session.set('errors', errors);
-    if (errors.hasError) {
-      return;
-    }
-    $(e.currentTarget).addClass("disabled");
-    var teacherId = Template.instance().cacheData.teacherId;
-    Meteor.call('reserveCourses', {'teacherId':teacherId, 'lessonCount':lessonCount, 'phases':phases}, function(err, result) {
-      if(err){
-        errors.lessonCount=err.reason;
-        Session.set('errors', errors);
-        $(e.currentTarget).removeClass("disabled");
-        return throwError(err.reason);
-      }
-      alert("约课成功");
-      $(e.currentTarget).removeClass("disabled");
-    });
   }
 });
---
+++ client/templates/teacher/schedule_weekly_for_order_test.js
@@ -0,0 +1,58 @@
+Template.scheduleWeeklyForOrderTest.helpers({
+  errorMessage: function(field) {
+    return Session.get('errors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('errors')[field] ? 'has-error' : '';
+  },
+  lessonCounts: function() {
+    var tct = TeacherAvailableTimes.findOne({"teacher.id": Template.instance().data.teacherId});
+    var availablePhases = (tct && tct.phases)?tct.phases:null;
+    return availablePhases?availablePhases.length:0;
+  }
+});
+Template.scheduleWeeklyForOrderTest.events({
+  'click td.phase': function(e) {
+    var ele=e.target, $ele = $(ele);
+    if ($ele.hasClass("available") && !$ele.hasClass('reserved')) {
+      $ele.toggleClass("chosen");
+    }
+  },
+  'click .btn-save': function(e) {
+    var lessonCount = parseInt($("input[name=lessonCount").val()), errors={hasError:false};
+    if (isNaN(lessonCount) || lessonCount<1) {
+      errors.lessonCount="课时数不能小于1";
+      errors.hasError=true;
+    }
+    var phases = [];
+    $("td.phase.chosen").each(function(){
+      $this = $(this);
+      if ($this.hasClass('reserved')) {
+        return;
+      }
+      i = $this.data('weekday'), start = $this.data('start'), end = $this.data('end');
+      phases.push({weekday:i, start:start, end:end})
+    });
+    console.log(phases);
+    if(phases.length==0) {
+      errors.phases="请点击上面方格选择时间段";
+      errors.hasError=true;
+    }
+    Session.set('errors', errors);
+    if (errors.hasError) {
+      return;
+    }
+    $(e.currentTarget).addClass("disabled");
+    var teacherId = Template.instance().data.teacherId;
+    Meteor.call('reserveCourses', {'teacherId':teacherId, 'lessonCount':lessonCount, 'phases':phases}, function(err, result) {
+      if(err){
+        errors.lessonCount=err.reason;
+        Session.set('errors', errors);
+        $(e.currentTarget).removeClass("disabled");
+        return throwError(err.reason);
+      }
+      alert("约课成功");
+      $(e.currentTarget).removeClass("disabled");
+    });
+  }
+});
