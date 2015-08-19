75d950255b1ba8c3766edc7f688533354adb1d32
Scott <zhonglijunyi@163.com>
2015-08-19 19:57:03 +0800

    order confirm page

--- client/templates/orders/order_course_confirm.js
+++ client/templates/orders/order_course_confirm.js
@@ -1,6 +1,43 @@
 Template.orderCourseConfirm.onCreated(function(){
   console.log(Session.get("orderTeacherId"));
 });
+Template.orderCourseConfirm.helpers({
+  myPhoneNo: function() {
+    var a = Meteor.user().phoneNo;
+    return a.substr(0,3)+'****'+a.substr(a.length-4);
+  },
+  timePhases: function() {
+    return Session.get("phases");
+  },
+  convMinutes2Str: function(mins) {
+    return ScheduleTable.convMinutes2Str(mins);
+  },
+  weekdayText: function(d) {
+    return '周'+ScheduleTable.dayNumWords[d];
+  },
+  courseCount: function() {
+    return Session.get("courseCount");
+  },
+  teacherName: function() {
+    var teacherId = Session.get("orderTeacherId");
+    var teacher = Meteor.users.findOne({_id:teacherId});
+    if (!teacher) {
+      return "教师姓名(Error)";
+    }
+    return teacher.profile.name;
+  },
+  courseSubject: function() {
+    return "年级-科目(TODO)";
+  },
+  teacherAvatarUrl: function() {
+    var teacherId = Session.get("orderTeacherId");
+    var teacher = Meteor.users.findOne({_id:teacherId});
+    if (!teacher) {
+      return "教师头像(Error)";
+    }
+    return teacher.profile.avatarUrl;
+  }
+});
 Template.orderCourseConfirm.events({
   'click #gotoPay': function(e) {
     Router.go('orderCoursePay');
