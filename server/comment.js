f0845c518feae9a8bc267a09df9a0a8c05eebfc6
jackyZhang <15957031@qq.com>
2015-09-01 17:18:38 +0800

    user ScheduleTable.attendanceStateDict['commented'] get commented

--- server/comment.js
+++ server/comment.js
@@ -13,7 +13,7 @@ Meteor.methods({
     //TODO with transaction
     Comment.insert(comment, function(error, result){
       if(!error){
-        CourseAttendances.update({_id: comment.courseAttendanceId}, {$set: {'state': 3}});
+        CourseAttendances.update({_id: comment.courseAttendanceId}, {$set: {'state': ScheduleTable.attendanceStateDict['commented'].value}});
         Meteor.users.update({_id: comment.teacher.id}, {$inc: {"profile.maScore": comment.maScore, "profile.maCount": 1, "profile.laScore": comment.laScore, "profile.laCount": 1}});
       }
     });
