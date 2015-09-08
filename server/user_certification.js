6aa71ca8ae6f979867c340d43a8d9137a003ddb6
Scott <zhonglijunyi@163.com>
2015-09-08 20:42:18 +0800

    app mine profile - editing certification pages

--- server/user_certification.js
+++ server/user_certification.js
@@ -13,5 +13,32 @@ Meteor.methods({
     var name = curUserProfile?curUserProfile.name:"";
     TeacherAudit.update({userId:Meteor.userId()},{$set:{'name':name,submitTime:now,certInfo:{submitTime:now, status: 'submited'}}},{upsert:true});
     // TODO：用户操作日志 UserOpLogs
+  },
+  uploadCertificationForApp: function(params) {
+    if (!params) {
+      throw new Meteor.Error('参数错误', '没有数据');
+    }
+    var setObj = {}, ok = false;
+    if (params['id'] && params['id'].url) {
+      setObj.idCardImgUrl = params.id.url;
+      ok = true;
+    }
+    if (params['edu'] && params['edu'].url) {
+      setObj.eduCertImgUrl = params['edu'].url;
+      ok = true;
+    }
+    if (params['teacher'] && params['teacher'].url) {
+      setObj.teacherCertImgUrl = params['teacher'].url;
+      ok = true;
+    }
+    if (params['profession'] && params['profession'].url) {
+      setObj.professionItems = [{'certImgUrl': params['profession'].url}];
+      ok = true;
+    }
+    if (!ok) {
+      throw new Meteor.Error('参数错误', '没有数据');
+    }
+    UserCertification.update({userId:Meteor.userId()},{$set:setObj},{upsert:true});
+    // TODO: 审核记录，用户操作日志
   }
 })
