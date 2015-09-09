c0151bb925a36327b0d8c83a119283325f6a18e4
jackyZhang <15957031@qq.com>
2015-09-09 18:19:49 +0800

    new orders page

--- client/templates/orders/orders.js
+++ client/templates/orders/orders.js
@@ -1,20 +1,43 @@
-Template.orders.onRendered(function () {
+Template.orders.onCreated(function(){
+  if(!Session.get('ionTab.current')){
+    Session.set('ionTab.current', "ordersNoPaid");
+  }
+});
+Template.orders.onRendered(function (){
   IonNavigation.skipTransitions = true;
+  $('.view').css("background-color","#FFFFFF");
 });
 Template.orders.helpers({
   empty: function(){
     return !this.orders || this.orders.count() === 0;
+  },
+  showOrders: function(){
+    var cutTab = Session.get('ionTab.current');
+    if(cutTab == 'ordersNoPaid'){
+      return Orders.find({"status": "submited", "student.id": Meteor.userId()}, this.terms.options);
+    }else if(cutTab == 'ordersPaidOk'){
+      return Orders.find({"status": {$in: ['paid', 'end']}, "student.id": Meteor.userId()}, this.terms.options);
+    }
+    return null;
+  },
+  orderTime: function(){
+    var momentTime = null;
+    return this.createdAt && (momentTime = moment(this.createdAt)) ? (new Date().getTime() - this.createdAt) < 79200000 ? momentTime.fromNow() : momentTime.fromNow() + " " + momentTime.format('HH:mm') : "";
+  },
+  courseHour: function(){
+    return this.hour ? this.hour : "";
+  },
+  payMoney: function(){
+    return this.cost ? accounting.formatNumber(this.cost, 2) : "";
   }
 });
 Template.orders.events({
   'click #findTeachers': function(e) {
     e.preventDefault();
-
-    Router.go("teachers");
+    Router.go("teachersFilter");
+  },
+  'click .order-detail-view-btn': function(e){
+    e.preventDefault();
+    Router.go("order", {id: this._id});
   }
 });
-Template.ionNavBar.events({
-  'click .btn-settings': function(e) {
-    Router.go("parentSettings");
-  }
-})
