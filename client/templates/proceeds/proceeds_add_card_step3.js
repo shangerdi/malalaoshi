0e376b3b59a53e7dd3a505c404bf4292e48f3457
Shang Erdi <shangerdi@malalaoshi.com>
2015-09-22 17:46:48 +0800

    Init proceeds templates

---
+++ client/templates/proceeds/proceeds_add_card_step3.js
@@ -0,0 +1,20 @@
+Template.proceedsAddCardStep3.helpers({
+  //add you helpers here
+});
+
+Template.proceedsAddCardStep3.events({
+  //add your events here
+});
+
+Template.proceedsAddCardStep3.onCreated(function() {
+  //add your statement here
+});
+
+Template.proceedsAddCardStep3.onRendered(function() {
+  //add your statement here
+});
+
+Template.proceedsAddCardStep3.onDestroyed(function() {
+  //add your statement here
+});
+
---
+++ client/templates/proceeds/proceeds_password.js
@@ -0,0 +1,20 @@
+Template.proceedsPassword.helpers({
+  //add you helpers here
+});
+
+Template.proceedsPassword.events({
+  //add your events here
+});
+
+Template.proceedsPassword.onCreated(function() {
+  //add your statement here
+});
+
+Template.proceedsPassword.onRendered(function() {
+  //add your statement here
+});
+
+Template.proceedsPassword.onDestroyed(function() {
+  //add your statement here
+});
+
