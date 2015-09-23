85635c485b78eec40a18d8fa2f33bdd6e16eeda8
jackyZhang <15957031@qq.com>
2015-09-23 19:48:54 +0800

    add normal distribution

--- client/templates/coupon/raffle.js
+++ client/templates/coupon/raffle.js
@@ -1,6 +1,9 @@
 var canRaffle = true;
+var couponId = '';
 Template.raffle.onCreated(function(){
   this.newRaffleValue = new ReactiveVar();
+  canRaffle = true;
+  couponId = '';
 });
 Template.raffle.events({
   'click .raffle-view > div > div': function(e){
@@ -20,6 +23,10 @@ Template.raffle.events({
         setRaffleValue(result, item.attr('id'));
        });
     }
+  },
+  'click #confirm': function(e){
+    e.preventDefault();
+    Router.go('couponConfirm', {'id': couponId});
   }
 });
 Template.raffle.helpers({
@@ -31,12 +38,24 @@ Template.raffle.helpers({
     if(ret){
       for(var i=0; i< ret.length; i++){
         if(ret[i].exist){
+          couponId = ret[i].id;
           return 3;
         }
       }
       return 1;
     }
     return 2;
+  },
+  proportion: function(){
+    var ret = Template.instance().newRaffleValue.get();
+    if(ret){
+      for(var i=0; i< ret.length; i++){
+        if(ret[i].select){
+          couponId = ret[i].id;
+          return accounting.formatNumber(ret[i].proportion * 100, 0);
+        }
+      }
+    }
   }
 });
 function removeRaffleImgShow(){
