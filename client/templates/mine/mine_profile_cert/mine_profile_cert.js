d569e6fa6e3e783104a6d6c3e30917150fd2b3f5
Scott <zhonglijunyi@163.com>
2015-09-28 20:12:20 +0800

    app mine cert-info audit status

--- client/templates/mine/mine_profile_cert/mine_profile_cert.js
+++ client/templates/mine/mine_profile_cert/mine_profile_cert.js
@@ -1,6 +1,28 @@
 var certUploadRoute = 'mineProfileCertUpload';
+var convStatus2Str = function(status) {
+  if (!status) {
+    return "未设置";
+  }
+  var statusDict = {'approved':"通过",'submited':"待审核",'rejected':"被驳回"};
+  var str = statusDict[status];
+  if (str) {
+    return str;
+  }
+  return "-";
+}
+var getAuditObj = function() {
+  if (!Meteor.userId()) return null;
+  return TeacherAudit.findOne({'userId': Meteor.userId()});
+}
 Template.mineProfileCert.helpers({
   getAuditStatusStr: function(field) {
+    var auditObj = getAuditObj();
+    if (auditObj) {
+      var certInfo = auditObj.certInfo;
+      if (certInfo) {
+        return convStatus2Str(certInfo.status);
+      }
+    }
     return '未设置';
   },
   pathForId: function() {
