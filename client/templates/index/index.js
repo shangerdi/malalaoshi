8cb3bc70ad120becab690106ffa3ddfbd088b163
jackyZhang <15957031@qq.com>
2015-09-28 20:27:20 +0800

    route to introduce

--- client/templates/index/index.js
+++ client/templates/index/index.js
@@ -16,11 +16,8 @@ Template.index.helpers({
     return [
       {
         imgSrc: "https://s3-ap-southeast-1.amazonaws.com/my.images.head/test_1234/1438334988731.png",
-        id: "HWQLWZ59GxtrdEoz3"
-      },
-      {
-        imgSrc: "https://s3-ap-southeast-1.amazonaws.com/my.images.head/12323232323/1437113067545.jpg",
-        id: "uCMX3NyPbpzvgNJn5"
+        id: "HWQLWZ59GxtrdEoz3",
+        thisPath: 'introduce'
       }
     ];
   },
